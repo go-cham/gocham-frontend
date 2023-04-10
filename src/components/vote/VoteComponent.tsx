fbcad79af9a2bba13ab8d300d4f749d8069e6f7e
Kid-Chang <pch3310@gmail.com>
2023-04-10 16:57:42 +0900

    fix(VoteComponent): pwa상황에 따른 크기 조정 설정
    추가적으로 개선이 필요한 부분: pwa를 감지하는 부분을 넓게 빼서, 전역적으로 이 변수를 이용할 수 있도록 하자.

--- src/components/vote/VoteComponent.tsx
+++ src/components/vote/VoteComponent.tsx
@@ -1,17 +1,23 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from "@emotion/react";
-import React from "react";
+import React, { useEffect } from "react";
 import VoteTitle from "./VoteTitle";
 import VoteContentComponent from "./VoteContentComponent";
+import styled from "@emotion/styled";
 
-const VoteComponentCss = css`
+type VoteComponentProps = {
+  isActivePWA: boolean;
+};
+const VoteComponentCss = styled.div<VoteComponentProps>`
   position: absolute;
   width: 34rem;
   height: 53.3rem;
   //top: 14rem;
-  top: 15vh;
+  //top: 15vh;
   // 세로 길이가 작은 폰때문에 vh로 조정
+
+  top: ${(props) => (props.isActivePWA ? "15vh" : "10vh")};
 
   background: #ffffff;
   /* Drop Shadow */
@@ -21,11 +27,25 @@ const VoteComponentCss = css`
 `;
 
 const VoteComponent = () => {
+  let activePwa: boolean = false;
+  useEffect(() => {
+    activePwa = isActivePWA();
+  }, []);
+  const isActivePWA = (): boolean => {
+    if (window.matchMedia("(display-mode: standalone)").matches) {
+      console.log("PWA 모드");
+      return true;
+    } else {
+      console.log("브라우저 모드. 컴퓨터로 들어가면 이상함");
+      return false;
+    }
+  };
   return (
-    <div css={VoteComponentCss}>
+    <VoteComponentCss isActivePWA={activePwa}>
+      {activePwa ? "PWA" : "브라우저"}
       <VoteTitle />
       <VoteContentComponent />
-    </div>
+    </VoteComponentCss>
   );
 };
 
