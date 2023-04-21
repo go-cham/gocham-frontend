fcf6e84a022e3cb03bdecc24690d57b07655ce8c
Kid-Chang <pch3310@gmail.com>
2023-04-22 00:51:25 +0900

    feat(login/register): 로그인 / 회원가입 navigate 관련 퍼블리싱 추가

--- src/pages/login/RegisterTerm.tsx
+++ src/pages/login/RegisterTerm.tsx
@@ -6,17 +6,22 @@ import react, { useEffect, useState } from "react";
 import { ButtonStyle } from "../../style/common";
 import palette from "../../style/color";
 import BackButton from "../../images/Common/back_button.png";
+import DetailArrow from "../../images/Login/detail_arrow.png";
 import CheckBox from "../../components/login/CheckBox";
+import { useNavigate } from "react-router-dom";
 
 export type AcceptType = {
   gochamTerm: boolean;
   personalInformation: boolean;
+  olderThan14: boolean;
   allCheck: boolean;
 };
 const RegisterTerm = () => {
+  const navigate = useNavigate();
   const [accept, setAccept] = useState<AcceptType>({
     gochamTerm: false,
     personalInformation: false,
+    olderThan14: false,
     allCheck: false,
   });
 
@@ -28,7 +33,11 @@ const RegisterTerm = () => {
 
   return (
     <RegisterTermWrap>
-      <img src={BackButton} alt={"뒤로가기"} />
+      <img
+        src={BackButton}
+        alt={"뒤로가기"}
+        onClick={() => navigate("/login")}
+      />
       <div className={"약관문구"}>
         환영합니다!
         <br />
@@ -44,6 +53,7 @@ const RegisterTerm = () => {
               setAccept({
                 gochamTerm: value,
                 personalInformation: value,
+                olderThan14: value,
                 allCheck: value,
               })
             }
@@ -57,6 +67,13 @@ const RegisterTerm = () => {
             setValue={(value) => setAccept({ ...accept, gochamTerm: value })}
           />
           [필수] 고민의 참견 이용약관 동의
+          <a
+            href={
+              "https://sharechang.notion.site/ac3f06fe803b497681f807f3df65fbe2"
+            }
+          >
+            <img src={DetailArrow} alt={"약관 상세"} className={"화살표"} />
+          </a>
         </CheckWrap>
         <CheckWrap>
           <CheckBox
@@ -66,6 +83,20 @@ const RegisterTerm = () => {
             }
           />
           [필수] 개인정보 수집 및 이용 동의
+          <a
+            href={
+              "https://sharechang.notion.site/c18f70f5ee40492fb8cdb89336014097"
+            }
+          >
+            <img src={DetailArrow} alt={"약관 상세"} className={"화살표"} />
+          </a>
+        </CheckWrap>
+        <CheckWrap>
+          <CheckBox
+            value={accept.olderThan14}
+            setValue={(value) => setAccept({ ...accept, olderThan14: value })}
+          />
+          [필수] 만 14세 이상 입니다.
         </CheckWrap>
       </section>
       {accept.gochamTerm && accept.personalInformation ? (
@@ -96,11 +127,17 @@ const RegisterTerm = () => {
 export default RegisterTerm;
 
 const CheckWrap = styled.div`
+  position: relative;
   height: 3.4rem;
   display: flex;
   align-items: center;
   font-weight: 500;
   font-size: 1.4rem;
+
+  & a {
+    position: absolute;
+    right: 0;
+  }
 `;
 
 const RegisterTermWrap = styled.div`
