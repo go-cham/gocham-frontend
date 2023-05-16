da6b7de9e5e6bed56e8038bc9f39bef9e9a0e5d6
Kid-Chang <pch3310@gmail.com>
2023-05-16 15:12:58 +0900

    fix(color): 팔레트 최신화

--- src/style/color.ts
+++ src/style/color.ts
@@ -4,13 +4,14 @@ const palette = {
   Accept: "#51AB7C",
   Error: "#EE7963",
   Background: "#F5F7FB",
+  White:"#FFF",
   Text1: "#2A2D37",
-  Text2: "#94969B",
-  Text3: "#b7b8bb",
-  Gray1: "#6A6C73",
-  Gray2: "#D4D5D7",
-  Gray3: "#EAEAEB",
-  Gray4: "#F4F4F5",
+  Text2: "#676A72",
+  Text3: "#8F9299",
+  Text4: "#B0B2B8",
+  Gray1: "#CCCFD4",
+  Gray2: "#E1E3E7",
+  Gray3: "#F4F4F5"
 };
 
 export default palette;
