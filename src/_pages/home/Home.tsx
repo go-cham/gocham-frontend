01f56bf748b2a64d2705856936a8fe38579ae800
Jihoo Kim <jihoo94@gmail.com>
2023-06-16 20:31:34 +0900

    fix(layout): 스크롤 시 컨텐츠가 헤더 위로 오버레이되는 현상

--- src/_pages/home/Home.tsx
+++ src/_pages/home/Home.tsx
@@ -1,27 +1,17 @@
 /** @jsxImportSource @emotion/react */
 
-import { css } from "@emotion/react";
 import React from "react";
 import PostListLayer from "../../_components/post/list/PostListLayer";
 import styled from "@emotion/styled";
 import palette from "../../style/color";
 import LogoAndTitle from "../../images/Common/LogoAndTitle.svg";
 
-const H1 = css`
-  margin-top: 1.8rem;
-  font-weight: 700;
-  font-size: 2.7rem;
-`;
-
 const Home = () => {
   return (
     <MainView>
       <div className={"title"}>
-        <h1 css={H1}>
-          <img src={LogoAndTitle} alt={"로고와타이틀"} />
-        </h1>
+        <img src={LogoAndTitle} alt={"로고와타이틀"} />
       </div>
-      <div style={{ height: "8.6rem" }} />
       <PostListLayer />
     </MainView>
   );
@@ -37,10 +27,8 @@ const MainView = styled.div`
   & .title {
     box-sizing: border-box;
     width: 100%;
-    position: absolute;
     background-color: white;
-    padding-left: 3rem;
-    padding-bottom: 2rem;
+    padding: 2rem 0 2rem 3rem;
     border-bottom: 0.1rem solid ${palette.Gray3};
     filter: drop-shadow(0px 0px 4px rgba(42, 45, 55, 0.1));
   }
