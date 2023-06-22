34efe3ec8f64e5c30c5e02852de235e82543a136
Jihoo Kim <jihoo94@gmail.com>
2023-06-22 13:34:23 +0900

    chore: tailwindcss prettier 플러그인

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,5 @@ module.exports = {
   importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)$', '^./(.*)$', '^../(.*)$'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
