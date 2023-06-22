2e12e8f4284f2dd7d6162b1ff5455310f4f06da5
Jihoo Kim <jihoo94@gmail.com>
2023-06-22 12:56:12 +0900

    style: tailwind 팔레트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f18721',
+        secondary: '#434343',
+        accept: '#51ab7c',
+        error: '#ee7963',
+        background: '#f5f7fb',
+        text1: '#2A2D37',
+        text2: '#676A72',
+        text3: '#8F9299',
+        text4: '#B0B2B8',
+        gray1: '#CCCFD4',
+        gray2: '#E1E3E7',
+        gray3: '#F4F4F5',
+      },
+    },
   },
   plugins: [],
 };
