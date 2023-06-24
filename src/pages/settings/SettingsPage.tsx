8e8188c4420627da129bda94cf0a471c848c2672
Jihoo Kim <jihoo94@gmail.com>
2023-06-24 14:12:13 +0900

    migration(tailwindcss):  설정 페이지 마이그레이션

--- src/pages/settings/SettingsPage.tsx
+++ src/pages/settings/SettingsPage.tsx
@@ -1,120 +1,86 @@
-/** @jsxImportSource @emotion/react */
-import styled from '@emotion/styled';
 import { useSetAtom } from 'jotai';
 import { useNavigate } from 'react-router-dom';
 
 import AppBar from '@/components/layout/AppBar';
 import { initialUserData, userAtom } from '@/states/userData';
-import palette from '@/styles/color';
-import { ButtonStyle } from '@/styles/common';
 import { appVersion } from '@/version';
 
 const SettingsPage = () => {
   const navigate = useNavigate();
   const setUserInfo = useSetAtom(userAtom);
 
-  const handleButtonClick = (goto: string) => {
-    if (goto === 'notice')
-      window.location.href =
-        'https://sharechang.notion.site/b422a175512e4710ba4c2ca2ebc8e035';
-    if (goto === 'terms')
-      window.location.href =
-        'https://sharechang.notion.site/ac3f06fe803b497681f807f3df65fbe2';
-    if (goto === 'informationTerm')
-      window.location.href =
-        'https://sharechang.notion.site/c18f70f5ee40492fb8cdb89336014097';
-    if (goto === 'quesition')
-      window.location.href =
-        'https://sharechang.notion.site/c18f70f5ee40492fb8cdb89336014097';
-    if (goto === 'logout') {
-      window.localStorage.removeItem('token');
-      navigate('/');
-      setUserInfo(initialUserData);
-    }
-    if (goto === 'deleteAccount')
-      alert('개발중입니다. 회원탈퇴는 관리자에게 연락해주세요.');
-  };
+  const SETTINGS_MENU: {
+    title: string;
+    link?: string;
+    onClick?: () => void;
+  }[] = [
+    {
+      title: '공지사항',
+      link: 'https://sharechang.notion.site/b422a175512e4710ba4c2ca2ebc8e035',
+    },
+    {
+      title: '이용약관',
+      link: 'https://sharechang.notion.site/ac3f06fe803b497681f807f3df65fbe2',
+    },
+    {
+      title: '개인정보 처리방침',
+      link: 'https://sharechang.notion.site/c18f70f5ee40492fb8cdb89336014097',
+    },
+    {
+      title: '문의하기',
+    },
+    {
+      title: 'FAQ',
+    },
+    {
+      title: '로그아웃',
+      onClick: () => {
+        localStorage.removeItem('token');
+        navigate('/');
+        setUserInfo(initialUserData);
+      },
+    },
+    {
+      title: '탈퇴하기',
+      onClick: () => {
+        alert('개발중입니다. 회원탈퇴는 관리자에게 연락해주세요.');
+      },
+    },
+    {
+      title: `버전: ${appVersion}`,
+    },
+    {
+      title: 'Copyright © go-cham Team. All rights reserved.',
+    },
+  ];
+
   return (
-    <SettingsWrap>
+    <div>
       <AppBar title={'설정'} />
-      <div className={'wrap'}>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('notice')}
-          style={{ borderTop: `0.1rem solid ${palette.Gray2}` }}
-        >
-          공지사항
-        </Button>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('terms')}
-        >
-          이용약관
-        </Button>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('informationTerm')}
-        >
-          개인정보 처리방침
-        </Button>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('question')}
-        >
-          문의하기
-        </Button>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('faq')}
-        >
-          FAQ
-        </Button>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('logout')}
-        >
-          로그아웃
-        </Button>
-        <Button
-          width={'100%'}
-          height={7.4}
-          onClick={() => handleButtonClick('deleteAccount')}
-        >
-          탈퇴하기
-        </Button>
-        <Button width={'100%'} height={7.4}>
-          버전: {appVersion}
-        </Button>
-        <Button width={'100%'} height={7.4}>
-          Copyright © go-cham Team. All rights reserved.
-        </Button>
-      </div>
-    </SettingsWrap>
+      <ul>
+        {SETTINGS_MENU.map((menu) => (
+          <li
+            key={menu.title}
+            className="flex h-[7.4rem] cursor-pointer items-center border-b-[1px] border-gray2 pl-[2.5rem] text-[1.6rem] font-medium"
+            onClick={menu.onClick}
+          >
+            {menu.link ? (
+              <a
+                href={menu.link}
+                rel="noreferrer"
+                target="_blank"
+                className="flex h-full w-full items-center"
+              >
+                {menu.title}
+              </a>
+            ) : (
+              menu.title
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
 export default SettingsPage;
-
-const SettingsWrap = styled.div`
-  //background-color: ${palette.Gray3};
-  position: relative;
-  width: 100%;
-  height: 100vh;
-`;
-
-const Button = styled(ButtonStyle)`
-  justify-content: left;
-  //border-top: 0.1rem solid ${palette.Gray2};
-  border-bottom: 0.1rem solid ${palette.Gray2};
-  padding-left: 2.5rem;
-  font-size: 1.6rem;
-  font-weight: 500;
-  background-color: ${palette.Background};
-  box-sizing: border-box;
-`;
