86dafdbbc6d227cf578a8306e88c0805fb6bf9e0
Jihoo Kim <jihoo94@gmail.com>
2023-06-24 12:40:55 +0900

    refactor: tailwind 커스텀 폰트 사이즈 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,6 @@ module.exports = {
     screens: {
       xl: '1300px',
     },
-    fontSize: {
-      xs: '1.2rem',
-      sm: '1.4rem',
-      base: '1.6rem',
-      lg: '1.8rem',
-      xl: '2.0rem',
-      '2xl': '2.2rem',
-    },
     extend: {
       colors: {
         primary: '#f18721',
