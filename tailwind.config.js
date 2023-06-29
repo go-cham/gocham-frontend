cef6f8fa44de209e227acb96b98e7e1a2f5014cc
Jihoo Kim <jihoo94@gmail.com>
2023-06-30 03:30:08 +0900

    feat: pretendard 폰트 설정 및 tailwind typography 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @types {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -122,9 +126,100 @@ module.exports = {
           },
         },
       },
+      fontSize: {
+        hero: [
+          '2.6rem',
+          {
+            lineHeight: '3.9rem',
+            latterSpacing: '0',
+            fontWeight: '700',
+          },
+        ],
+        heading1: [
+          '2rem',
+          {
+            lineHeight: '3rem',
+            latterSpacing: '-0.6px',
+            fontWeight: '700',
+          },
+        ],
+        heading2: [
+          '1.8rem',
+          {
+            lineHeight: '2.7rem',
+            latterSpacing: '-0.54px',
+            fontWeight: '700',
+          },
+        ],
+        heading3: [
+          '1.6rem',
+          {
+            lineHeight: '2rem',
+            fontWeight: '700',
+          },
+        ],
+        subheading: [
+          '1.4rem',
+          {
+            lineHeight: '2.1rem',
+            fontWeight: '700',
+          },
+        ],
+        body5: [
+          '1.6rem',
+          {
+            lineHeight: '2.4rem',
+            latterSpacing: '-0.48px',
+            fontWeight: '500',
+          },
+        ],
+        body4: [
+          '1.4rem',
+          {
+            lineHeight: '2.1rem',
+            latterSpacing: '-0.42px',
+            fontWeight: '500',
+          },
+        ],
+        body3: [
+          '1.4rem',
+          {
+            lineHeight: '2.1rem',
+            latterSpacing: '-0.42px',
+            fontWeight: '400',
+          },
+        ],
+        body2: [
+          '1.2rem',
+          {
+            lineHeight: '1.8rem',
+            latterSpacing: '-0.36px',
+            fontWeight: '500',
+          },
+        ],
+        body1: [
+          '1.2rem',
+          {
+            lineHeight: '1.8rem',
+            latterSpacing: '-0.36px',
+            fontWeight: '400',
+          },
+        ],
+        caption: [
+          '1rem',
+          {
+            lineHeight: '1.5rem',
+            latterSpacing: '-0.3px',
+            fontWeight: '500',
+          },
+        ],
+      },
       backgroundImage: {
         'with-character': "url('src/images/background.png')",
         'without-character': "url('src/images/background_nocharVer.png')",
+      },
+      fontFamily: {
+        pretendard: ['Pretendard', ...fontFamily.sans],
       },
     },
   },
