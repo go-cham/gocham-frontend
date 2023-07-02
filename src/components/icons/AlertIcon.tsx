6b9639db7c42332283737358b75cb8cc6e210d44
Jihoo Kim <jihoo94@gmail.com>
2023-07-03 01:14:17 +0900

    feat: NicknameInput 컴포넌트 추가

---
+++ src/components/icons/AlertIcon.tsx
@@ -0,0 +1,38 @@
+export default function AlertIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="alert-circle 1">
+        <path
+          id="Vector"
+          d="M16 26C21.5228 26 26 21.5228 26 16C26 10.4772 21.5228 6 16 6C10.4772 6 6 10.4772 6 16C6 21.5228 10.4772 26 16 26Z"
+          stroke="#FF5664"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M16 11L16 16"
+          stroke="#FF5664"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          id="Vector_3"
+          d="M16 20H16.01"
+          stroke="#FF5664"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
