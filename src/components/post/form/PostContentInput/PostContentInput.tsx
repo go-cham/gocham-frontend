102f61ae550d262ac3fa0a34849df6f5ecc710e4
Jihoo Kim <jihoo94@gmail.com>
2023-07-03 12:35:26 +0900

    refactor: PostContentInput

--- src/components/post/form/PostContentInput/PostContentInput.tsx
+++ src/components/post/form/PostContentInput/PostContentInput.tsx
@@ -1,46 +1,48 @@
 import { ChangeEvent, useState } from 'react';
-import { UseFormRegisterReturn } from 'react-hook-form';
 
 import { twMergeCustom } from '@/libs/tw-merge';
 
+const MAX_LENGTH = 280;
+
 interface PostContentInputProps {
-  error?: string | null;
-  register?: UseFormRegisterReturn;
+  onChange?: (content: string) => void;
+  errorMessage?: string | null;
   className?: string;
 }
 
 export default function PostContentInput({
-  error,
-  register,
+  onChange,
+  errorMessage,
   className,
 }: PostContentInputProps) {
-  const MAX_LENGTH = 280;
-  const [enteredContent, setEnteredContent] = useState('');
+  const [content, setContent] = useState('');
 
-  const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setEnteredContent(e.target.value);
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newContent = e.target.value;
+    setContent(newContent);
+    onChange && onChange(newContent);
   };
 
   return (
     <div className={twMergeCustom('flex w-[34rem] flex-col', className)}>
       <label className="text-subheading">내용</label>
       <textarea
-        {...register}
         rows={6}
         maxLength={280}
         placeholder={`최대 ${MAX_LENGTH}자 입력`}
         className={twMergeCustom(
           'mt-[1.1rem] resize-none border-b-[0.2rem] border-custom-gray-500 bg-transparent pb-[0.7rem] text-body4 text-custom-text-900 placeholder:text-body3 placeholder:text-custom-text-400 focus:-mb-[0.2rem] focus:border-b-[0.4rem] focus:border-custom-gray-800',
-          error &&
+          errorMessage &&
             'border-custom-semantic-warn-500 focus:border-custom-semantic-warn-500'
         )}
-        onChange={handleContentChange}
+        onChange={handleChange}
+        value={content}
       />
       <div className="mt-[0.7rem] flex items-center justify-between">
         <span className="text-body1 text-custom-semantic-warn-500">
-          {error || ''}
+          {errorMessage || ''}
         </span>
-        <span className="self-end text-body1 text-custom-text-400">{`${enteredContent.length}/${MAX_LENGTH}`}</span>
+        <span className="self-end text-body1 text-custom-text-400">{`${content.length}/${MAX_LENGTH}`}</span>
       </div>
     </div>
   );
