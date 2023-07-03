3bb82d72365d808670960914f1af9bd7487fff6a
Jihoo Kim <jihoo94@gmail.com>
2023-07-03 13:13:07 +0900

    fix: storybook eslint 모듈 못 찾음

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,39 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:storybook/recommended'
+  ],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'
-  // '@tanstack/query'
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    // '@tanstack/query'
   ],
 
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/no-unknown-property': ['error', {
-      ignore: ['css']
-    }],
-    'react/prop-types': 'off'
-  }
+    'react/no-unknown-property': [
+      'error',
+      {
+        ignore: ['css'],
+      },
+    ],
+    'react/prop-types': 'off',
+  },
 };
