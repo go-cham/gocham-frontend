4a81b65ea5d7c5d8972b2e9b9970fa7814e99517
Jihoo Kim <jihoo94@gmail.com>
2023-07-06 15:35:13 +0900

    feat: 닉네임 중복 검사

--- src/components/user/CollectNicknameAgeGender/CollectNicknameAgeGender.tsx
+++ src/components/user/CollectNicknameAgeGender/CollectNicknameAgeGender.tsx
@@ -1,9 +1,12 @@
+import { AxiosError } from 'axios';
 import { FormEvent, useState } from 'react';
 
+import useUser from '@/apis/hooks/users/useUser';
 import BirthdayInput from '@/components/register/form/BirthdayInput';
 import { Birthday } from '@/components/register/form/BirthdayInput/BirthdayInput';
 import NicknameInput from '@/components/register/form/NicknameInput';
 import Button from '@/components/ui/buttons/Button';
+import { axiosInstance } from '@/libs/axios';
 import { Gender } from '@/types/user';
 import { validateBirthday } from '@/utils/validations/birthday';
 import { validateNickname } from '@/utils/validations/nickname';
@@ -26,10 +29,28 @@ export default function CollectNicknameAgeGender({
     day: '',
   });
   const [gender, setGender] = useState<Gender | null>(null);
+  const [nicknameErrorMessage, setNicknameErrorMessage] = useState<
+    string | null
+  >(null);
+  const { user } = useUser();
 
-  const handleNicknameInputChange = (nickname: string) => {
+  const handleNicknameInputChange = async (nickname: string) => {
     setIsDirty((prev) => ({ ...prev, nickname: true }));
     setNickname(nickname);
+
+    let error = validateNickname(nickname);
+    if (!error) {
+      try {
+        await axiosInstance.get(`/user/${user?.id}/duplicated-nickname`, {
+          params: { nickname },
+        });
+      } catch (e) {
+        if (e instanceof AxiosError && e.response?.status === 400) {
+          error = '이미 존재하는 닉네임입니다.';
+        }
+      }
+    }
+    setNicknameErrorMessage(error);
   };
 
   //생년월일 관련
@@ -60,9 +81,6 @@ export default function CollectNicknameAgeGender({
     onSubmit(nickname, formattedBirthday, gender);
   };
 
-  const nicknameErrorMessage = isDirty.nickname
-    ? validateNickname(nickname)
-    : null;
   const nicknameSuccessMessage =
     isDirty.nickname && !nicknameErrorMessage
       ? '사용 가능한 닉네임입니다.'
