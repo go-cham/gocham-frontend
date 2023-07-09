e7eaab32ab805a0c1773cf0601802c52210cef2c
Jihoo Kim <jihoo94@gmail.com>
2023-07-09 23:48:16 +0900

    feat: home 페이지에서 로고 클릭시 맨위로 스크롤

--- src/pages/home/HomeHeader/HomeHeader.tsx
+++ src/pages/home/HomeHeader/HomeHeader.tsx
@@ -1,9 +1,19 @@
 import LogoAndTitle from '@/images/Common/LogoAndTitle.svg';
 
 export default function HomeHeader() {
+  const handleTitleClick = () => {
+    const list = document.querySelector('#home-post-list');
+    if (list) {
+      list.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <header className="bg-white py-[1.2rem] pl-[2.5rem]">
-      <img src={LogoAndTitle} alt={'로고'} />
+      <img src={LogoAndTitle} alt={'로고'} onClick={handleTitleClick} />
     </header>
   );
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -6,7 +6,10 @@ export default function HomePage() {
   return (
     <div className="flex h-full flex-col bg-white">
       <HomeHeader />
-      <main className="hide-scrollbar flex-1 overflow-y-scroll px-[2.5rem] pb-40 pt-[0.8rem]">
+      <main
+        id="home-post-list"
+        className="hide-scrollbar flex-1 overflow-y-scroll px-[2.5rem] pb-40 pt-[0.8rem]"
+      >
         <PostCardList />
       </main>
       <BottomAppBar />
