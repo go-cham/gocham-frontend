f3d601f9a1bde9546e7f0b48de0896082861886b
Jihoo Kim <jihoo94@gmail.com>
2023-07-10 21:54:18 +0900

    style: scrollbar 보이기

--- src/pages/feed/PostDetail/PostDetail.tsx
+++ src/pages/feed/PostDetail/PostDetail.tsx
@@ -207,7 +207,7 @@ function PostDetailContent({
         )}
         {images && images.length > 1 && (
           <div
-            className="hide-scrollbar flex space-x-[1.3rem] overflow-x-scroll px-[1.5rem]"
+            className="flex space-x-[1.3rem] overflow-x-auto px-[1.5rem]"
             style={{
               scrollSnapType: 'x mandatory',
             }}
@@ -217,7 +217,7 @@ function PostDetailContent({
                 key={image}
                 src={image}
                 alt={'게시글이미지'}
-                className="pointer-events-none h-[29.3rem] max-w-[29.3rem] rounded-[0.5rem] object-cover"
+                className="h-[29.3rem] max-w-[29.3rem] rounded-[0.5rem] object-cover"
                 style={{
                   scrollSnapAlign: 'center',
                 }}
