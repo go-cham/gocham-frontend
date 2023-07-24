b7882740942d5ec16071e91e187452f6cbd696b2
Jihoo Kim <jihoo94@gmail.com>
2023-07-25 00:12:32 +0900

    style: 메인 로고 cursor pointer 제거

--- src/pages/home/HomeHeader/HomeHeader.tsx
+++ src/pages/home/HomeHeader/HomeHeader.tsx
@@ -20,12 +20,7 @@ export default function HomeHeader() {
     <header className="flex items-center py-[1.2rem] pl-[2.5rem]">
       {!hasUploaded ? (
         <>
-          <img
-            src={LogoStarve}
-            alt={'배고픈로고'}
-            onClick={handleTitleClick}
-            className="cursor-pointer"
-          />
+          <img src={LogoStarve} alt={'배고픈로고'} onClick={handleTitleClick} />
           <div
             className="ml-[1.5rem] rounded-lg pb-[0.7rem] pl-[1.1rem] pr-[1.7rem] pt-[0.6rem] shadow-dropdown font-custom-subheading"
             style={{
@@ -37,12 +32,7 @@ export default function HomeHeader() {
         </>
       ) : (
         <>
-          <img
-            src={LogoFull}
-            alt={'배부른로고'}
-            onClick={handleTitleClick}
-            className="cursor-pointer"
-          />
+          <img src={LogoFull} alt={'배부른로고'} onClick={handleTitleClick} />
           <div
             className="ml-[1.5rem] rounded-lg pb-[0.7rem] pl-[1.1rem] pr-[1.7rem] pt-[0.6rem] shadow-dropdown font-custom-subheading"
             style={{
