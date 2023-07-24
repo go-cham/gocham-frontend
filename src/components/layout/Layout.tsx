e5613bce8e11d19fc42167aea42451a54b9f76f7
Jihoo Kim <jihoo94@gmail.com>
2023-07-25 00:11:06 +0900

    fix: 백그라운드 이미지 반응형

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,11 +6,11 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex h-full w-screen overflow-hidden bg-without-character bg-cover bg-center xl:bg-with-character">
+    <div className="flex h-full w-screen overflow-hidden bg-without-character bg-cover bg-center">
       <img
         src={BackgroundText}
         alt="백그라운드 텍스트"
-        className="fixed left-[10rem] top-[10rem] h-[39.3rem] w-[43.5rem]"
+        className="fixed left-[10rem] top-[10rem] hidden h-[39.3rem] w-[43.5rem] xl:block"
       />
       <div className="relative mx-auto h-full w-full max-w-[43rem] bg-background-mainBg-0 xl:left-[25rem]">
         {children}
