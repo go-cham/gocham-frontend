5c3f21f5041c6dd7c838714b5c9477f197722575
Jihoo Kim <jihoo94@gmail.com>
2023-08-01 03:42:40 +0900

    fix: 탈퇴하기 delete 버튼 위치

--- src/components/post/form/PostVoteInput/PostVoteInput.tsx
+++ src/components/post/form/PostVoteInput/PostVoteInput.tsx
@@ -65,30 +65,71 @@ export default function PostVoteInput({
         readOnly && 'pointer-events-none bg-background-voteBg-100'
       )}
     >
-      <input
-        id={id}
-        ref={inputRef}
-        type="text"
-        onFocus={() => {
-          if (item) setShowReset(true);
-        }}
-        onBlur={() => {
-          setTimeout(() => {
-            setShowReset(false);
-          }, 0);
-        }}
-        maxLength={maxLength}
-        placeholder={placeholder}
-        onChange={handleChange}
-        value={item}
-        readOnly={readOnly}
-        className={twMerge(
-          'w-full rounded-[0.5rem] border border-background-dividerLine-300 bg-transparent py-[1.2rem] pl-[1.3rem] font-system-body4 placeholder:text-text-subExplain-400 placeholder:font-system-body3 group-focus-within:border-text-subTitle-700',
-          hasError &&
-            'border-semantic-warn-500 group-focus-within:border-semantic-warn-500',
-          readOnly && 'text-text-explain-500'
-        )}
-      />
+      <div className="relative">
+        <input
+          id={id}
+          ref={inputRef}
+          type="text"
+          onFocus={() => {
+            if (item) setShowReset(true);
+          }}
+          onBlur={() => {
+            setTimeout(() => {
+              setShowReset(false);
+            }, 0);
+          }}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          onChange={handleChange}
+          value={item}
+          readOnly={readOnly}
+          className={twMerge(
+            'w-full rounded-[0.5rem] border border-background-dividerLine-300 bg-transparent py-[1.2rem] pl-[1.3rem] font-system-body4 placeholder:text-text-subExplain-400 placeholder:font-system-body3 group-focus-within:border-text-subTitle-700',
+            hasError &&
+              'border-semantic-warn-500 group-focus-within:border-semantic-warn-500',
+            readOnly && 'text-text-explain-500'
+          )}
+        />
+        <div className="absolute right-[1.2rem] top-1/2 flex -translate-y-1/2 space-x-[0.8rem]">
+          {showReset && (
+            <button onFocus={() => setShowReset(true)}>
+              <DeleteIcon
+                className="h-[1.6rem] w-[1.6rem] cursor-pointer rounded-full bg-background-button-300"
+                color="white"
+                onClick={handleReset}
+              />
+            </button>
+          )}
+          {image && (
+            <div className="h-[4rem] w-[4rem]">
+              <img
+                src={image}
+                alt="투표 이미지"
+                className="h-full w-full object-cover"
+              />
+              {!readOnly && (
+                <button onClick={onDeleteImage}>
+                  <DeleteIcon
+                    className="absolute right-0 top-0 h-[1.6rem] w-[1.6rem] bg-[#676a72]"
+                    color="white"
+                  />
+                </button>
+              )}
+            </div>
+          )}
+          {!image && onUploadImage && (
+            <label>
+              <ImageFileIcon className="cursor-pointer" />
+              <input
+                type="file"
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageChange}
+              />
+            </label>
+          )}
+        </div>
+      </div>
       {errorMessage && (
         <div className="w-full text-right">
           <span className="text-semantic-warn-500 font-system-body1">
@@ -96,45 +137,6 @@ export default function PostVoteInput({
           </span>
         </div>
       )}
-      <div className="absolute right-[1.2rem] top-1/2 flex -translate-y-1/2 space-x-[0.8rem]">
-        {showReset && (
-          <button onFocus={() => setShowReset(true)}>
-            <DeleteIcon
-              className="h-[1.6rem] w-[1.6rem] cursor-pointer rounded-full bg-background-button-300"
-              color="white"
-              onClick={handleReset}
-            />
-          </button>
-        )}
-        {image && (
-          <div className="h-[4rem] w-[4rem]">
-            <img
-              src={image}
-              alt="투표 이미지"
-              className="h-full w-full object-cover"
-            />
-            {!readOnly && (
-              <button onClick={onDeleteImage}>
-                <DeleteIcon
-                  className="absolute right-0 top-0 h-[1.6rem] w-[1.6rem] bg-[#676a72]"
-                  color="white"
-                />
-              </button>
-            )}
-          </div>
-        )}
-        {!image && onUploadImage && (
-          <label>
-            <ImageFileIcon className="cursor-pointer" />
-            <input
-              type="file"
-              accept="image/*"
-              className="hidden"
-              onChange={handleImageChange}
-            />
-          </label>
-        )}
-      </div>
     </div>
   );
 }
