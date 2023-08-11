aafd1730368156e99d801ba6eb435c97460ac70b
Jihoo Kim <jihoo94@gmail.com>
2023-08-11 17:25:11 +0900

    test: react testing library 셋업

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,14 @@
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 describe('test', () => {
   it('test', () => {
-    expect(true).toBe(true);
+    render(<TestComponent message={'Testing'} />);
+
+    expect(screen.getByText(/Testing/i)).toBeDefined();
   });
 });
+
+function TestComponent({ message }: { message: string }) {
+  return <h1>{message}</h1>;
+}
--- vite.config.js
+++ vite.config.js
@@ -26,4 +26,8 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
