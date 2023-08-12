230b7d22a03ac27fbd57893327627d6e7875b9e6
Jihoo Kim <jihoo94@gmail.com>
2023-08-13 02:01:25 +0900

    chore: 1시간 주기로 sw update 확인

--- src/hooks/useUpdate.tsx
+++ src/hooks/useUpdate.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import { useRegisterSW } from 'virtual:pwa-register/react';
 
+const UPDATE_INTERVAL = 60 * 60 * 1000;
+
 export default function useUpdate() {
   const [showUpdate, setShowUpdate] = useState(false);
   const { updateServiceWorker } = useRegisterSW({
+    onRegistered: (r) => {
+      r &&
+        setInterval(() => {
+          r.update();
+        }, UPDATE_INTERVAL);
+    },
     onNeedRefresh: () => {
       setShowUpdate(true);
     },
