373a0e4d56d309d282cf4c19c250470a6fe31465
Jihoo Kim <jihoo94@gmail.com>
2023-08-27 19:01:23 +0900

    fix: manifest 인식 버그

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,52 @@ export default defineConfig({
       },
       srcDir: 'src',
       filename: 'serviceWorker.ts',
-      manifestFilename: 'manifest.json',
+      manifest: {
+        short_name: '고민의 참견',
+        name: '고민의 참견, 고참',
+        icons: [
+          {
+            src: 'icon/android/icon-36.png',
+            sizes: '36x36',
+            type: 'image/png',
+          },
+          {
+            src: 'icon/android/icon-48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: 'icon/android/icon-72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'icon/android/icon-96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'icon/android/icon-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'icon/android/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'icon/android/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: './index.html',
+        display: 'standalone',
+        theme_color: '#F5F7FB',
+        background_color: '#ffffff',
+      },
     }),
   ],
   resolve: {
