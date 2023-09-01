b69dd94dbf7f3f697b7a9d0ccc7b84d9ad51cedd
Jihoo Kim <jihoo94@gmail.com>
2023-09-01 12:55:42 +0900

    fix: 스켈레톤 패딩 수정

--- src/common/components/ui/skeleton/PostSummarySkeleton/PostSummarySkeleton.tsx
+++ src/common/components/ui/skeleton/PostSummarySkeleton/PostSummarySkeleton.tsx
@@ -8,12 +8,7 @@ export default function PostSummarySkeleton({
   className,
 }: PostSummarySkeletonProps) {
   return (
-    <div
-      className={twMerge(
-        'w-full bg-white px-[2.5rem] pb-[2.3rem] pt-[2.1rem]',
-        className,
-      )}
-    >
+    <div className={twMerge('w-full bg-white pb-[2.3rem]', className)}>
       <div className="flex items-center space-x-[0.5rem]">
         <div className="h-[2.5rem] w-[2.5rem] rounded-full bg-background-subBg-100" />
         <div className="h-[1.8rem] w-[11rem] rounded-[3px] bg-background-subBg-100" />
--- src/features/posts/components/post-card/PostCardListSkeleton/PostCardListSkeleton.tsx
+++ src/features/posts/components/post-card/PostCardListSkeleton/PostCardListSkeleton.tsx
@@ -2,7 +2,7 @@ import PostCardSkeleton from '@/common/components/ui/skeleton/PostCardSkeleton';
 
 export default function PostCardListSkeleton() {
   return (
-    <ul className="flex w-full flex-col items-center space-y-[1.7rem] px-[2.5rem]">
+    <ul className="flex w-full flex-col items-center space-y-[1.7rem]">
       {Array(10)
         .fill(null)
         .map((_, index) => (
